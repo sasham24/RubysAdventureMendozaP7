379448060a818d625e2e16ca7716ad60a18a4ae0
sasham24 <736149@student.dusd.net>
2023-11-17 15:25:09 -0800

    added animations
    added animations to enemy and ruby

--- P7RubysAdventureMendoza/Assets/Scripts/RubyController.cs
+++ P7RubysAdventureMendoza/Assets/Scripts/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
    
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get {return currentHealth; } }
     int currentHealth;
 
@@ -17,13 +17,17 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-   
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2 (1, 0);
+ 
     // Start is called before the first frame update
     void Start()
     {
-      rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        
+        animator = GetComponent<Animator>();
+
 
     }
 
@@ -32,6 +36,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical"); 
+
+      
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x , move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -55,6 +71,8 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
             {
                 return; 
