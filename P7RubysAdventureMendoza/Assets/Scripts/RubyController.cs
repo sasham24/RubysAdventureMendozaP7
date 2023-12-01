ec3f6af70b9703a714284b203513fcb6511a812b
sasham24 <736149@student.dusd.net>
2023-11-30 21:44:02 -0800

    health
    added Ui health bar

--- P7RubysAdventureMendoza/Assets/Scripts/RubyController.cs
+++ P7RubysAdventureMendoza/Assets/Scripts/RubyController.cs
@@ -84,7 +84,7 @@ public class RubyController : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth);
     }
 
     void Launch ()
